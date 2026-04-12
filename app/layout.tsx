d67fd5aa621db473836012a1c5b2a9d15d2b9e3b
Selim <ahmetselimden@gmail.com>
2026-04-12 21:26:33 +0300

    Fix GTM loading strategy

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import Script from "next/script";
 import PageViewTracker from "@/components/PageViewTracker";
 import "./globals.css";
 
-const GTM_ID = "GTM-TS2575P9";
 const siteUrl = "https://www.itirazdilekcesi.com";
 const siteName = "itirazdilekcesi.com";
 const defaultTitle = "Trafik Cezası İtiraz Dilekçesi Hazırla";
@@ -94,32 +93,22 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
-        <script
+        <Script
+          id="gtm-init"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id=GTM-TS2575P9'+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TS2575P9');`,
           }}
         />
-        <Script
-          src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            window.dataLayer.push({
-              'gtm.start': new Date().getTime(),
-              event: 'gtm.js'
-            });
-          `}
-        </Script>
       </head>
       <body className="bg-shell font-body text-ink antialiased">
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TS2575P9"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
